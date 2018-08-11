7f2311c8f343571bbc392a9f03462db5aebdab57
chrissetiana <chrissetiana@gmail.com>
2018-08-11 23:18:17 +0100

    Reset lifecycle events

--- app/src/main/java/com/chrissetiana/lifecycle/MainActivity.java
+++ app/src/main/java/com/chrissetiana/lifecycle/MainActivity.java
@@ -1,8 +1,9 @@
 package com.chrissetiana.lifecycle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,47 +23,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-
         logEvent("onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
         logEvent("onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
         logEvent("onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-
         logEvent("onStop");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-
         logEvent("onRestart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
         logEvent("onDestroy");
     }
 
     private void logEvent(String event) {
         lifeCycleDisplay.append(event + "\n");
         Log.d("MainActivity", "Event: " + event);
+    }
+
+    public void resetEvent(View view) {
+        lifeCycleDisplay.setText("Lifecycle callbacks:\n");
     }
 }
